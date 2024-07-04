cf05aa822ad515c980adcaf8a5d743c66c9c43f8
MessiasFCM <messiasferes127@gmail.com>
2024-07-04 20:01:47 -0300

    Update time

--- src/time_utils.c
+++ src/time_utils.c
@@ -1,12 +1,21 @@
-#include "../include/time_utils.h"
+#include <stdlib.h>
 #include <stdio.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include "../include/time.h"
 
-void calculateExecutionTime(struct timeval start, struct timeval end, struct rusage usageStart, struct rusage usageEnd) {
-    long seconds = end.tv_sec - start.tv_sec;
-    long microseconds = end.tv_usec - start.tv_usec;
-    double elapsed = seconds + microseconds*1e-6;
+// Função para calcular e exibir o tempo de execução do programa
+void calculateExecutionTime(struct timeval start, struct timeval end,
+                            struct rusage usageStart, struct rusage usageEnd)
+{
+    double elapsed = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1e6;
+    double userTime = (usageEnd.ru_utime.tv_sec - usageStart.ru_utime.tv_sec) +
+                      (usageEnd.ru_utime.tv_usec - usageStart.ru_utime.tv_usec) / 1e6;
+    double sysTime = (usageEnd.ru_stime.tv_sec - usageStart.ru_stime.tv_sec) +
+                     (usageEnd.ru_stime.tv_usec - usageStart.ru_stime.tv_usec) / 1e6;
 
-    printf("Elapsed time: %.6f seconds\n", elapsed);
-    printf("User time: %ld.%06ld\n", usageEnd.ru_utime.tv_sec - usageStart.ru_utime.tv_sec, usageEnd.ru_utime.tv_usec - usageStart.ru_utime.tv_usec);
-    printf("System time: %ld.%06ld\n", usageEnd.ru_stime.tv_sec - usageStart.ru_stime.tv_sec, usageEnd.ru_stime.tv_usec - usageStart.ru_stime.tv_usec);
+    printf("\n# Tempo de execução:\n");
+    printf("-> Tempo total de execução: %.3f segundos.\n", elapsed);
+    printf("-> Tempo de usuário: %.3f segundos.\n", userTime);
+    printf("-> Tempo de sistema: %.3f segundos.\n", sysTime);
 }
