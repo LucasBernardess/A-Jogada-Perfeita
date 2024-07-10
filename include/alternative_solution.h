fc777ed23b71c8d3e974462becd8920551021cc4
MessiasFCM <messiasferes127@gmail.com>
2024-07-09 23:50:30 -0300

    Update

--- include/alternative_solution.h
+++ include/alternative_solution.h
@@ -1,7 +1,6 @@
 #ifndef ALTERNATIVE_SOLUTION_H
 #define ALTERNATIVE_SOLUTION_H
 
-unsigned long int maxAlternative(unsigned long int a, unsigned long int b);
 unsigned long int maxPointsBruteForce(unsigned long int* a, int n, int i);
 
 #endif
--- include/dp_solution.h
+++ include/dp_solution.h
@@ -1,7 +1,6 @@
 #ifndef DP_SOLUTION_H
 #define DP_SOLUTION_H
 
-unsigned long int max(unsigned long int a, unsigned long int b);
 unsigned long int maxPointsDP(unsigned long int* a, int n);
 
 #endif
